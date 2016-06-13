1b959479aa0278c401d2c67ebbd8ec04321605d6
Silvius <silviusko@gmail.com>
2016-06-13 14:08:56 +0800

    @Update decryption/encryption methods
    - reference from "Security "Crypto" provider deprecated in Android N"
    http://android-developers.blogspot.tw/2016/06/security-crypto-provider-deprecated-in.html

--- toolkit/src/main/java/tt/kao/toolkit/utils/HashUtils.java
+++ toolkit/src/main/java/tt/kao/toolkit/utils/HashUtils.java
@@ -1,33 +1,38 @@
 package tt.kao.toolkit.utils;
 
+import android.support.annotation.NonNull;
 import android.text.TextUtils;
 import android.util.Base64;
 
-import java.io.UnsupportedEncodingException;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
-import java.util.Arrays;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
 
-import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 
 /**
+ *
+ * reference from https://nelenkov.blogspot.tw/2012/04/using-password-based-encryption-on.html
+ *
  * @author luke_kao
+ *
  */
 public class HashUtils {
-    private static final String IV_KEY = "68091a4e07156d6aa17b7ac88a92102d";
-    private static final String PASSWORD = "GBAQUFXV4uJJxxkyoIkD";
-    private static final int KEY_SIZE = 16;
+    private static final String DEFAULT_PASSWORD = "GBAQUFXV4uJJxxkyoIkD";
+    private static final String SEPARATOR = "]";
+    private static final int ITERATION_COUNT = 1000;
+    private static final int KEY_LENGTH = 128;
+    private static final int SALT_LENGTH = KEY_LENGTH / 8;
 
     public static String encrypt(String content) {
-        return encrypt(content, PASSWORD);
+        return encrypt(content, DEFAULT_PASSWORD);
     }
 
     public static String encrypt(String content, String password) {
@@ -35,8 +40,25 @@ public class HashUtils {
 
         try {
             byte[] byteContent = content.getBytes("utf-8");
-            byte[] cryptContent = crypto(byteContent, password, Cipher.ENCRYPT_MODE);
-            return toBase64(cryptContent);
+
+            byte[] salt = new byte[SALT_LENGTH];
+
+            SecureRandom random = new SecureRandom();
+            random.nextBytes(salt);
+
+            SecretKeySpec key = deriveKeyPbkdf2(password, salt);
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            byte[] iv = new byte[cipher.getBlockSize()];
+            random.nextBytes(iv);
+
+            IvParameterSpec ivParams = new IvParameterSpec(iv);
+
+            cipher.init(Cipher.ENCRYPT_MODE, key, ivParams);
+
+            byte[] cryptContent = cipher.doFinal(byteContent);
+
+            return toBase64(salt) + SEPARATOR + toBase64(iv) + SEPARATOR + toBase64(cryptContent);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -44,20 +66,39 @@ public class HashUtils {
     }
 
     public static String decrypt(String content) {
-        return decrypt(content, PASSWORD);
+        return decrypt(content, DEFAULT_PASSWORD);
     }
 
     public static String decrypt(String content, String password) {
         if (TextUtils.isEmpty(content)) return "";
 
         try {
-            byte[] decryptFrom = fromBase64(content);
-            byte[] decryptContent = crypto(decryptFrom, password, Cipher.DECRYPT_MODE);
-            return new String(decryptContent, "utf-8");
+            String[] fields = content.split(SEPARATOR);
+            byte[] salt = fromBase64(fields[0]);
+            byte[] iv = fromBase64(fields[1]);
+            byte[] cipherBytes = fromBase64(fields[2]);
+
+            SecretKey key = deriveKeyPbkdf2(password, salt);
+            IvParameterSpec ivParams = new IvParameterSpec(iv);
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            cipher.init(Cipher.DECRYPT_MODE, key, ivParams);
+            byte[] plaintext = cipher.doFinal(cipherBytes);
+
+            return new String(plaintext, "UTF-8");
         } catch (Exception e) {
             e.printStackTrace();
         }
         return "";
+    }
+
+    @NonNull
+    private static SecretKeySpec deriveKeyPbkdf2(String password, byte[] salt) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        KeySpec keySpec = new PBEKeySpec(password.toCharArray(), salt, ITERATION_COUNT, KEY_LENGTH);
+        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        byte[] keyByte = keyFactory.generateSecret(keySpec).getEncoded();
+
+        return new SecretKeySpec(keyByte, "AES");
     }
 
     public static String md5(String content) {
@@ -76,31 +117,6 @@ public class HashUtils {
         }
 
         return "";
-    }
-
-    private static synchronized byte[] crypto(byte[] content, String password, int mode)
-        throws NoSuchAlgorithmException, UnsupportedEncodingException, NoSuchPaddingException, InvalidKeyException,
-        IllegalBlockSizeException, BadPaddingException, NoSuchProviderException, InvalidAlgorithmParameterException {
-
-        byte[] key = generateKey(password, KEY_SIZE);
-        SecretKeySpec keySpec = new SecretKeySpec(key, "AES");
-
-        byte[] ivKey = generateKey(IV_KEY, KEY_SIZE);
-        IvParameterSpec ivParameterSpec = new IvParameterSpec(ivKey);
-
-
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        cipher.init(mode, keySpec, ivParameterSpec);
-
-        return cipher.doFinal(content);
-    }
-
-    private static byte[] generateKey(String value, int size) throws NoSuchAlgorithmException, UnsupportedEncodingException {
-        MessageDigest digest = MessageDigest.getInstance("SHA-1");
-
-        byte[] sha1 = digest.digest(value.getBytes("UTF-8"));
-
-        return Arrays.copyOf(sha1, size);
     }
 
     private static String toBase64(byte[] bytes) {
